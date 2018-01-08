d00c72e8b2c67a314ea2e8f969f217d013676846
humosanti <hsmolip@gmail.com>
2018-01-08 17:43:10 -0500

    First program finished

--- myPerl.pl
+++ myPerl.pl
@@ -1,6 +1,15 @@
 #!/usr/bin/perl
-#
+=for
+Program myPerl.pl mi primer programa escrito en perl
+ 
+Written by Humberto Molinares
+Copyright 2018
+
+
+
+=cut
+
 use strict;
-#
-#
+
+
 print("Hello. World!");
